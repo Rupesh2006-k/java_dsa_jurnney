ec27f076ede6d939e942cd2d616e733dab9d1a21
Rupesh kushwaha <rupeshkushwaha03032@gmail.com>
2026-06-18 08:14:47 +0530

    method is done

--- src/method/StaticVSNonStatic.java
+++ src/method/StaticVSNonStatic.java
@@ -1,4 +1,22 @@
 package method;
 
 public class StaticVSNonStatic {
+    //non static method
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+
+    // static method
+    public static void sayBy() {
+        System.out.println("ciao ciao");
+    }
+
+    public static void main(String[] args) {
+//object createion
+        StaticVSNonStatic st = new StaticVSNonStatic();
+        st.sayHello();
+        System.out.println("Hey");
+        sayBy();
+    }
+
 }
