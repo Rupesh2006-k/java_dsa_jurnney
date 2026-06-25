488dde91004d75747c65d32007eb30b3a3270fc3
Rupesh kushwaha <rupeshkushwaha03032@gmail.com>
2026-06-25 20:00:07 +0530

    bubble sort is complete

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,69 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int arr[] = {1, 8, 5, 6, 7, 3, 4, 2};
+
+        bubbleSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean isSwaped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    isSwaped = true;
+                }
+            }
+
+            System.out.println("Pass " + (i + 1) + ": " + isSwaped);
+
+            if (!isSwaped) {
+                break;
+            }
+        }
+    }
+}
+
+//package sorting;
+//
+//import java.util.Arrays;
+//
+//public class BubbleSort {
+//    public static void main(String[] args) {
+//        int arr[] = {1, 8, 5, 6, 7, 3, 4, 2};
+//        bubbleSort(arr);
+//        System.out.println(Arrays.toString(arr));
+//    }
+//
+//    public static void bubbleSort(int arr[]) {
+//        int n = arr.length;
+//        boolean isSwaped = false;
+//        for (int i = 0; i < n - 1; i++) {
+//
+//            for (int j = 0; j < n - 1 - i; j++) {
+//                if (arr[j] > arr[j + 1]) {
+//                    int temp = arr[j];
+//                    arr[j] = arr[j + 1];
+//                    arr[j + 1] = temp;
+//                    isSwaped = true;
+//                }
+//                if (!isSwaped) break;
+//            }
+//
+//            System.out.println(isSwaped);
+//
+//        }
+//    }
+//}
