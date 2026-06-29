27af7807511450a77fb29365b39a5cd3db3daf36
RUPESH KUSHWAHA <lagendryvide@gmail.com>
2026-06-29 20:48:58 +0530

    Time: 1 ms (2.30%) | Memory: 43.7 MB (68.99%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+
+                // Swap matrix[i][j] with matrix[j][i]
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse every row
+        for (int i = 0; i < n; i++) {
+
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+
+                // Swap left and right elements
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        // Print rotated matrix
+        for (int i = 0; i < n; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+            return;
+        }
+    }
+}
